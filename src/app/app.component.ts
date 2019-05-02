f8a1c413d6ca5ade08149c30af6cd24e08d0df7a
Bukixo <bukithompson@gmail.com>
2019-05-02 20:11:20 +0100

    name of country appears when clicked on the pin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { LocationsDataService } from './locations-data.service'
-
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +9,44 @@ import { LocationsDataService } from './locations-data.service'
 })
 
 
-export class AppComponent {
+// export class AppComponent {
   
 
+//    locations : any
 
+
+//   constructor(public locationsData : LocationsDataService) {
+//     this.locationsData = locationsData
+//   }
+// locations = [
+//   {
+//   "name": "Åland Islands",
+//   "capital": "Mariehamn",
+//   "latitude": 60.116667,
+//   "longitude": 19.9
+//   },
+//   {
+//   "name": "Albania",
+//   "capital": "Tirana",
+//   "latitude": 41,
+//   "longitude": 20
+//   },
+//   {
+//   "name": "Andorra",
+//   "capital": "Andorra la Vella",
+//   "latitude": 42.5,
+//   "longitude": 1.5
+//   }
+
+// ]
+  
+export class AppComponent {
+  
   constructor(public locationsData : LocationsDataService) {
     this.locationsData = locationsData
   }
 locations = this.locationsData.getLocations()
   
 
-  
 
 }
