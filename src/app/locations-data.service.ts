c51777a5a7aa32510c7fdd4759d38582d044b49b
Bukixo <bukithompson@gmail.com>
2019-05-03 14:43:53 +0100

    fixed cors issue

--- src/app/locations-data.service.ts
+++ src/app/locations-data.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Location } from './location.model'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'  
 })
 export class LocationsDataService {
   apiUrl = 'https://s3-eu-west-1.amazonaws.com/omnifi/techtests/locations.json'
@@ -12,6 +12,10 @@ export class LocationsDataService {
 
 
   getLocations() {
+
+    
     return this.http.get<Location[]>(this.apiUrl);
   }
 }
+
+
